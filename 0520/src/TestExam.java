6fc2d002138ee89870037f078d265dbe045b6f84
Kimhyunwoo <dndn0726@naver.com>
2021-05-20 15:04:52 +0900

    0520 프로젝트 추가

---
+++ 0520/src/TestExam.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class TestExam {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a;
+		System.out.println("500 이하의 자연수를 입력하세요.");
+		for(a=1;a<500;a++){
+		}
+		
+	}
+
+}
